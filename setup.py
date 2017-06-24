b610de24203bb229ae86d33c899b00bc31a19469
rocky <rb@dustyfeet.com>
2017-06-24 05:45:18 -0400

    We need Python xdis 3.4.0 or greater

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ from setuptools import setup, find_packages
 setup(
        classifiers        = classifiers,
        description        = short_desc,
-       install_requires   = ['click'],
+       install_requires   = ['click', 'xdis >= 3.4.0'],
        license            = license,
        long_description   = long_description,
        maintainer         = maintainer,
