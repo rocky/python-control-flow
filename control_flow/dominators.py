40ceae7051854252d9bf09a552bb5c8171bc0101
rocky <rb@dustyfeet.com>
2017-06-24 18:09:45 -0400

    Mostly indentation...
    But look at dominance frontier.

--- control_flow/dominators.py
+++ control_flow/dominators.py
@@ -16,120 +16,127 @@ class DominatorTree(object):
   """
 
   def __init__(self, cfg):
-    self.cfg = cfg
-    self.doms = {}
-    self.df = {}
-    self.build()
+      self.cfg = cfg
+      self.doms = {}
+      self.df = {}
+      self.build()
 
 
   def build(self):
-    graph = self.cfg.graph
-    entry = self.cfg.entry_node
-    self.build_dominators(graph, entry)
-    # self.build_df(graph)
+      graph = self.cfg.graph
+      entry = self.cfg.entry_node
+      self.build_dominators(graph, entry)
+      # self.build_df(graph)
 
 
   def build_dominators(self, graph, entry):
-    """
-      Builds the dominator tree based on:
-        http://www.cs.rice.edu/~keith/Embed/dom.pdf
+      """
+        Builds the dominator tree based on:
+          http://www.cs.rice.edu/~keith/Embed/dom.pdf
 
-      Also used to build the post-dominator tree.
-    """
-    doms = self.doms
-    doms[entry] = entry
-    post_order = dfs_postorder_nodes(graph, entry)
+        Also used to build the post-dominator tree.
+      """
+      doms = self.doms
+      doms[entry] = entry
+      post_order = dfs_postorder_nodes(graph, entry)
 
-    post_order_number = {}
-    i = 0
-    for n in post_order:
-      post_order_number[n] = i
-      i += 1
+      post_order_number = {}
+      i = 0
+      for n in post_order:
+          post_order_number[n] = i
+          i += 1
 
-    def intersec(b1, b2):
-      finger1 = b1
-      finger2 = b2
-      po_finger1 = post_order_number[finger1]
-
-      if finger2 in post_order_number.keys():
-        po_finger2 = post_order_number[finger2]
-      else:
-        po_finger2 = None
-
-      while po_finger1 != po_finger2:
-        no_solution = False
-        while po_finger1 < po_finger2:
-          finger1 = doms.get(finger1, None)
-          if finger1 is None:
-            no_solution = True
-            break
+      def intersec(b1, b2):
+          finger1 = b1
+          finger2 = b2
           po_finger1 = post_order_number[finger1]
-        while po_finger2 < po_finger1:
-          finger2 = doms.get(finger2, None)
-          if finger2 is None:
-            no_solution = True
-            break
-          po_finger2 = post_order_number[finger2]
-        if no_solution:
-          break
-      return finger1
 
-    changed = True
-    while changed:
-      changed = False
-      for b in reversed(post_order):
-        if b == entry:
-          continue
-        predecessors = b.predecessors
-        new_idom = next(iter(predecessors))
-        for p in predecessors:
-          if p == new_idom:
+          if finger2 in post_order_number.keys():
+              po_finger2 = post_order_number[finger2]
+          else:
+              po_finger2 = None
+
+          while po_finger1 != po_finger2:
+              no_solution = False
+              while po_finger1 < po_finger2:
+                  finger1 = doms.get(finger1, None)
+                  if finger1 is None:
+                      no_solution = True
+                      break
+                  po_finger1 = post_order_number[finger1]
+              while po_finger2 < po_finger1:
+                  finger2 = doms.get(finger2, None)
+                  if finger2 is None:
+                     no_solution = True
+                     break
+                  po_finger2 = post_order_number[finger2]
+              if no_solution:
+                  break
+          return finger1
+
+      changed = True
+      while changed:
+        changed = False
+        for b in reversed(post_order):
+          if b == entry:
             continue
-          if p in doms:
-            new_idom = intersec(p, new_idom)
-        if b not in doms or doms[b] != new_idom:
-          doms[b] = new_idom
-          changed = True
+          predecessors = b.predecessors
+          new_idom = next(iter(predecessors))
+          for p in predecessors:
+            if p == new_idom:
+              continue
+            if p in doms:
+              new_idom = intersec(p, new_idom)
+          if b not in doms or doms[b] != new_idom:
+            doms[b] = new_idom
+            changed = True
+            pass
           pass
-        pass
-    return
+      return
 
   def build_df(self, graph):
-    """
-      Builds the dominance frontier.
-    """
-    doms = self.doms
-    df = self.df
+      """
+        Builds the dominance frontier.
+      """
+      doms = self.doms
+      df = self.df
 
-    for b in graph.nodes:
-      df[b] = set()
+      for b in graph.nodes:
+          df[b] = set()
 
-    for b in graph.nodes:
-      predecessors = b.bb.predecessors
-      if len(predecessors) > 1:
-        for p in predecessors:
-          runner = p
-          while runner != doms[b.bb]:
-            df[runner].add(b)
-            runner = doms[runner]
+      for b in graph.nodes:
+        predecessors = b.bb.predecessors
+        if len(predecessors) > 1:
+            for p in predecessors:
+                runner = p
+                while runner != doms[b.bb]:
+                    if runner in df:
+                        df[runner].add(b)
+                    else:
+                        df[runner] = set([b])
+                        break
+                    runner = doms[runner]
+                pass
+            pass
+      pass
 
   def tree(self):
-    g_nodes = {}
-    doms = self.doms
-    g = DiGraph()
+      g_nodes = {}
+      doms = self.doms
+      g = DiGraph()
 
-    for node in doms:
-      if node not in g_nodes:
-        cur_node = g.make_add_node(node)
-        g_nodes[node] = cur_node
-      cur_node = g_nodes[node]
+      for node in doms:
+          if node not in g_nodes:
+              cur_node = g.make_add_node(node)
+              g_nodes[node] = cur_node
+          cur_node = g_nodes[node]
 
-      parent = doms.get(node, None)
-      if parent is not None and parent != node:
-        if parent not in g_nodes:
-          parent_node = g.make_add_node(parent)
-          g_nodes[parent] = parent_node
-        parent_node = g_nodes[parent]
-        g.make_add_edge(parent_node, cur_node, 'fall-through')
+          parent = doms.get(node, None)
+          if parent is not None and parent != node:
+              if parent not in g_nodes:
+                  parent_node = g.make_add_node(parent)
+                  g_nodes[parent] = parent_node
+              parent_node = g_nodes[parent]
+              g.make_add_edge(parent_node, cur_node, 'fall-through')
 
-    return g
+      return g
