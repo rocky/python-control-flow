d1e60eb1e5a341ebce78fe40df555d8c9bff4d1a
rocky <rb@dustyfeet.com>
2025-10-11 17:37:12 -0400

    More packaging and package testing

---
+++ admin-tools/install-all-newest.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+python_control_flow_owd=$(pwd)
+bs=${BASH_SOURCE[0]}
+mydir=$(dirname $bs)
+python_control_flow_fulldir=$(readlink -f $mydir)
+cd $python_control_flow_fulldir
+. ./checkout_common.sh
+
+pyenv_file="pyenv-newest-versions"
+if ! source $pyenv_file ; then
+    echo "Having trouble reading ${pyenv_file} version $(pwd)"
+    exit 1
+fi
+
+cd ../dist/
+
+install_check_command="python-cfg --version"
+install_file="python_control_flow-1.0.0.tar.gz"
+for pyversion in $PYVERSIONS; do
+    echo "*** Installing ${install_file} for Python ${pyversion} ***"
+    pyenv local $pyversion
+    pip install $install_file
+    $install_check_command
+    echo "----"
+done
