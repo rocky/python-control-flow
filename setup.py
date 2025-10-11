907a9630438df9bd8ee432b1709d63b340ee43ea
rocky <rb@dustyfeet.com>
2025-10-11 12:49:12 -0400

    Packaging for 3.8-3.10

--- setup.py
+++ setup.py
@@ -1,7 +1,70 @@
 #!/usr/bin/env python
 
-"""Setup script for the 'python_control_flow' distribution."""
+"""Setup script for the 'control_flow' distribution."""
 
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-setup()
+classifiers = [
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Topic :: Software Development :: Code Generators",
+]
+
+# The rest in alphabetic order
+entry_points = {"console_scripts": ["python-cfg=control_flow.__main__:main"]}
+ftp_url = None
+license = "GPL2"
+maintainer = "Rocky Bernstein"
+maintainer_email = "rb@dustyfeet.com"
+modname = "python_control_flow"
+name = "python-control-flow"
+py_modules = ["python_control_flow"]
+short_desc = "Control Flow Toolkit"
+web = "https://github.com/rocky/python-control-flow"
+
+
+def get_srcdir():
+    filename = os.path.normcase(os.path.dirname(os.path.abspath(__file__)))
+    return os.path.realpath(filename)
+
+
+srcdir = get_srcdir()
+
+
+def read(*rnames):
+    return open(os.path.join(srcdir, *rnames)).read()
+
+
+# Get info from files; set: long_description and VERSION
+long_description = read("README.rst") + "\n"
+
+# Version is overwritten by the below exec(read())
+__version__ = "??"
+exec(read("python_control_flow/version.py"))
+
+
+setup(
+    classifiers=classifiers,
+    description=short_desc,
+    entry_points=entry_points,
+    install_requires=["click", "xdis >= 6.1.1"],
+    license=license,
+    long_description=long_description,
+    maintainer=maintainer,
+    maintainer_email=maintainer_email,
+    packages=find_packages(),
+    py_modules=py_modules,
+    name=name,
+    test_suite="nose.collector",
+    url=web,
+    tests_require=["nose>=1.0"],
+    version=__version__,
+)
